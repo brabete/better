9be3eb831ae5f2cb447322a8e206923636b0a49b
Andrei Brabete <andreibrabete@gmail.com>
2019-06-09 11:31:52 +0100

    Initial application generated by JHipster-6.0.1

---
+++ src/main/java/io/github/better/application/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package io.github.better.application.config.audit;
---
+++ src/main/java/io/github/better/application/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package io.github.better.application.config;
